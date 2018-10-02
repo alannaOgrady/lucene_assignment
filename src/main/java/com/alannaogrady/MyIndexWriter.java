119ef256485809791cfbc307c073710abbc0a2df
Alanna O'Grady <alannasogrady@gmail.com>
2018-10-02 16:51:20 +0100

    Initial commit

---
+++ src/main/java/com/alannaogrady/MyIndexWriter.java
@@ -0,0 +1,66 @@
+package com.alannaogrady;
+// java.io.BufferedReader;
+import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.nio.charset.StandardCharsets;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+
+public class MyIndexWriter 
+{
+    private static MyIndexWriter instance = null;
+
+    private MyIndexWriter() {
+      // Exists only to defeat instantiation.
+    }
+    public static MyIndexWriter getInstance() {
+      if(instance == null) {
+         instance = new MyIndexWriter();
+      }
+      return instance;
+    }
+
+
+    public Directory index() throws IOException, ParseException {
+        // 0. Specify the analyzer for tokenizing text.
+        //    The same analyzer should be used for indexing and searching
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+
+        // 1. create the index - not saved to disc.. just temporary
+        Directory index = new RAMDirectory();
+
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+
+        IndexWriter w = new IndexWriter(index, config);
+        addDoc(w, "Lucene in Action", "193398817");
+        addDoc(w, "Lucene for Dummies", "55320055Z");
+        addDoc(w, "Managing Gigabytes", "55063554A");
+        addDoc(w, "The Art of Computer Science", "9900333X");
+        w.close();
+
+        return index;
+
+    }
+
+   
+
+    private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
+        Document doc = new Document();
+        doc.add(new TextField("title", title, Field.Store.YES));
+
+        // use a string field for isbn because we don't want it tokenized
+        doc.add(new StringField("isbn", isbn, Field.Store.YES));
+        w.addDocument(doc);
+    }
+}
